ba718549ff954c8d3f9f1e626ef0bd59456981de
Michael Ries <michael@riesd.com>
2015-05-12 16:00:36 -0600

    initial

---
+++ lib/protobuf/core.rb
@@ -0,0 +1,7 @@
+require "protobuf/core/version"
+
+module Protobuf
+  module Core
+    # Your code goes here...
+  end
+end
---
+++ lib/protobuf/core/version.rb
@@ -0,0 +1,5 @@
+module Protobuf
+  module Core
+    VERSION = "0.1.0"
+  end
+end
